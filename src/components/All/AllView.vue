9794c1b040475952f48f30de897c39db008715df
HenryWu1113 <bug910284@gmail.com>
2023-04-27 00:18:26 +0800

    接取單字api

--- src/components/All/AllView.vue
+++ src/components/All/AllView.vue
@@ -1,16 +1,16 @@
 <template>
   <n-input v-model:value="keyword" placeholder="搜尋單字" clearable></n-input>
   <div class="wordCardWrap mt-5 text-white">
-    <WordCard />
+    <WordCard v-for="item in words" :key="item" :data="item" />
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 import WordCard from '@/components/WordCard.vue'
-
+import { api } from '@/plugins/axios'
 const keyword = ref('')
-
+const words = ref([])
 onMounted(() => {
   console.log('onMounted')
 })
@@ -18,4 +18,16 @@ onMounted(() => {
 onBeforeUnmount(() => {
   console.log('onBeforeUnmount')
 })
+
+const getFilterWords = async () => {
+  try {
+    const { data } = await api.get('/words')
+    console.log(data)
+    words.value = [...data.result]
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getFilterWords()
 </script>
--- src/components/WordCard.vue
+++ src/components/WordCard.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="wordCard border border-white p-3 rounded-[5px] flex items-center">
-    <p class="text-[16px] border-r pr-3">食べ物</p>
-    <p class="text-[16px] px-3">たべもの</p>
+    <p class="text-[16px] border-r pr-3">{{ data.word }}</p>
+    <p class="text-[16px] px-3">{{ data.hiragana }}</p>
     <div class="border border-white w-[20px] h-[20px] flex items-center justify-center mr-auto">
-      <p class="text-[12px]">2</p>
+      <p class="text-[12px]">{{ data.tone }}</p>
     </div>
     <n-icon :component="CaretForwardCircle" size="22" class="cursor-pointer"></n-icon>
   </div>
 </template>
 
+<style lang="scss" scoped>
+.wordCard + .wordCard {
+  margin-top: 10px;
+}
+</style>
+
 <script setup>
 import { CaretForwardCircle } from '@vicons/ionicons5'
+
+const props = defineProps(['data'])
+console.log(props.data)
 </script>
